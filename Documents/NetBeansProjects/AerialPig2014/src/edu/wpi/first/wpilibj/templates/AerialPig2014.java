5d5a8ca2628907db838009fc3371baab2a3634c5
Zach1902 <zachch1902@gmail.com>
2014-02-05 19:11:15 -0500

    AerialPig2014 Robot Code

---
+++ Documents/NetBeansProjects/AerialPig2014/src/edu/wpi/first/wpilibj/templates/AerialPig2014.java
@@ -0,0 +1,54 @@
+ /*
+  __   ____  ____  __   __   __      ____  __  ___ 
+ / _\ (  __)(  _ \(  ) / _\ (  )    (  _ \(  )/ __)
+/    \ ) _)  )   / )( /    \/ (_/\   ) __/ )(( (_ \
+\_/\_/(____)(__\_)(__)\_/\_/\____/  (__)  (__)\___/
+FRC Code for the 2014 robot
+Written by:
+Tyler Felsted :D
+Phillip Parker
+Bryce John-Michael "The Bryceter" Dere
+Anderson Perkins
+Garry Little
+Sebastian Hedge
+
+ */
+package edu.wpi.first.wpilibj.templates;
+
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.DigitalOutput;
+
+public class AerialPig2014 extends IterativeRobot
+{
+    
+    public void robotInit()
+    {
+
+    }
+    
+    public void autonomousInit()
+    {
+        
+    }
+
+    public void autonomousPeriodic()
+    {
+
+    }
+    
+    public void teleopInit()
+    {
+        
+    }
+
+    public void teleopPeriodic()
+    {
+        
+    }
+    
+    public void testPeriodic()
+    {
+    
+    }
+}
