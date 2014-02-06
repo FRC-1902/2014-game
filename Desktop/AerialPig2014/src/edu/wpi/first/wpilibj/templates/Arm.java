9d17e8eea6a921ea480ab9932c19732591cd3ae7
Zach1902 <zachch1902@gmail.com>
2014-02-05 19:21:29 -0500

    AerialPig2014 Robot Code

---
+++ Desktop/AerialPig2014/src/edu/wpi/first/wpilibj/templates/Arm.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates;
+import edu.wpi.first.wpilibj.*;
+/**
+ *
+ * @author Developer
+ */
+public class Arm {
+    
+    Talon armTalon1;
+    Talon armTalon2;
+    Talon rollerTalon;
+    Solenoid gripperSolenoid;
+    Encoder armEncoder;
+    DigitalInput gripperTouchSensor;
+    DigitalInput topArmLimit;
+    DigitalInput bottomArmLimit;
+    
+    public Arm(int armTal1, int armTal2, int rollTal, int armEnc1, int armEnc2, int gripSolenoid, int touch, int topTouch, int bottomTouch)
+    {
+        armTalon1 = new Talon(armTal1);
+        armTalon2 = new Talon(armTal2);
+        rollerTalon = new Talon(rollTal);
+        
+        armEncoder = new Encoder(armEnc1, armEnc2);
+        
+        gripperSolenoid = new Solenoid(gripSolenoid);
+        
+        gripperTouchSensor = new DigitalInput(touch);
+        topArmLimit = new DigitalInput(topTouch);
+        bottomArmLimit = new DigitalInput(bottomTouch);
+    }
+    
+    public void setArmMotors(double arm)
+    {
+        armTalon1.set(-arm);
+        armTalon2.set(arm);
+    }
+    
+    public void setIntakeRollers(double roll)
+    {
+        rollerTalon.set(roll);
+    }
+    
+    public boolean getGripperTouchSensor()
+    {
+        return gripperTouchSensor.get();
+    }
+    
+    public boolean getTopArmLimit()
+    {
+        return topArmLimit.get();
+    }
+    
+    public boolean getBottomArmLimit()
+    {
+        return bottomArmLimit.get();
+    }
+    
+    public void setGripperSolenoid(boolean gripper)
+    {
+        gripperSolenoid.set(gripper);
+    }
+    
+    public void startArmEncoder(boolean setEnc)
+    {
+        if(setEnc)
+        {
+            armEncoder.start();
+        }
+        else if(!setEnc)
+        {
+            armEncoder.stop();
+        }
+    }
+    
+    public void resetArmEncoder()
+    {
+        armEncoder.reset();
+    }
+    
+    public int getArmEncoder()
+    {
+        return armEncoder.get();
+    }
+    
+    //THIS MUST BE TESTED
+    public void moveArmTo(int targetPosition)
+    {
+        double p = 0.05; //This needs to be tuned
+        double error = targetPosition - getArmEncoder();
+        
+        error *= p;
+        
+        setArmMotors(error);
+    }
+}
---
+++ Desktop/AerialPig2014/src/edu/wpi/first/wpilibj/templates/ControlStation.java
@@ -0,0 +1,148 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates;
+import edu.wpi.first.wpilibj.*;
+import java.util.*;
+
+/**
+ *
+ * @author Developer
+ */
+
+public class ControlStation 
+{
+    
+    DriverStation control;
+    
+    int fireButton = 1;//digital
+    int winchButton = 2;//digital
+    int clawButton = 3;//digital
+    int floorPreset = 4;//digital
+    int scorePreset = 5;//digital
+    int trussPreset = 6;//digital
+    
+    int gripperSwitch = 1;//analog
+    
+    Joystick leftJoystick;
+    Joystick rightJoystick;
+    Joystick armJoystick;
+    
+    
+    
+    public static final int NOT_PRESSED = 0;
+    public static final int PRESSED = 1;
+    
+    public static final int UP = 1;
+    public static final int DOWN = 0;
+    
+    //Check these values!
+    public static final int LEFT = -1;
+    public static final int MIDDLE = 0;
+    public static final int RIGHT = 1;
+    
+    public void init()
+    {
+        control = DriverStation.getInstance();
+    }
+    
+    public double getLeftJoystick()
+    {
+        return leftJoystick.getY();
+    }
+    
+    public double getRightJoystick()
+    {
+        return rightJoystick.getY();
+    }
+    
+    public double getArmJoystick()
+    {
+        return armJoystick.getY();
+    }
+    
+    public int getTrigger()
+    {
+        if(leftJoystick.getTrigger() || rightJoystick.getTrigger())
+        {
+            return PRESSED;
+        }
+        else
+        {
+            return NOT_PRESSED;
+        }
+    }
+    
+    public int getFireButton()
+    {
+        if(control.getDigitalIn(fireButton))
+        {
+            return PRESSED;
+        }
+        else
+        {
+            return NOT_PRESSED;
+        }
+    }
+    
+    public int getWinchButton()
+    {
+        if(control.getDigitalIn(winchButton))
+        {
+            return PRESSED;
+        }
+        else
+        {
+            return NOT_PRESSED;
+        }
+    }
+    
+    public int getFloorPreset()
+    {
+        return control.getDigitalIn(floorPreset)? PRESSED : NOT_PRESSED;
+    }
+    
+    public int getScorePreset()
+    {
+        return control.getDigitalIn(scorePreset)? PRESSED : NOT_PRESSED;
+    }
+    
+    public int getTrussPreset()
+    {
+        return control.getDigitalIn(trussPreset)? PRESSED : NOT_PRESSED;
+    }
+    
+    public int getGripperSwitch()
+    {
+        if(control.getDigitalIn(clawButton))
+        {
+            return UP;
+        }
+        else
+        {
+            return DOWN;
+        }
+    }
+    
+    public int getIntakeSwitch()
+    {
+        //We need to check these values when we get the drivers station running
+        double switchValue = control.getAnalogIn(gripperSwitch);
+        if(switchValue < 0.7)
+        {
+            return LEFT;
+        }
+        else if(switchValue > 0.8 && switchValue < 2.0)
+        {
+            return MIDDLE;
+        }
+        else if(switchValue > 2.5)
+        {
+            return RIGHT;
+        }
+        return MIDDLE;
+    }
+}
